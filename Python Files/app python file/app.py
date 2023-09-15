8037102c2a871ddc1b604d6eabc2c05e0f8d0af5
Anmol Kumar <128449996+anmolkumarfromspn@users.noreply.github.com>
2023-09-15 18:24:10 +0530

    Add files via upload

---
+++ Python Files/app python file/app.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import movieposters as mp
+
+def recommend(title):
+    movie_index = movies[movies['title'] == title].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+    recommended_movies = []
+    for i in movies_list:
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+movies_dict = pickle.load(open('movies.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+
+st.title("Movie Recommender System")
+
+option = st.selectbox("Please choose a movie to get recommendations",
+                      movies['title'].values)
+
+if st.button("Recommend"):
+    recommendations = recommend(option)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+
+        st.image(mp.get_poster(title=recommendations[0]))
+        st.text(recommendations[0])
+
+    with col2:
+
+        st.image(mp.get_poster(title=recommendations[1]))
+        st.text(recommendations[1])
+
+    with col3:
+
+        st.image(mp.get_poster(title=recommendations[2]))
+        st.text(recommendations[2])
+
+    with col4:
+
+        st.image(mp.get_poster(title=recommendations[3]))
+        st.text(recommendations[3])
+
+    with col5:
+
+        st.image(mp.get_poster(title=recommendations[4]))
+        st.text(recommendations[4])
+
